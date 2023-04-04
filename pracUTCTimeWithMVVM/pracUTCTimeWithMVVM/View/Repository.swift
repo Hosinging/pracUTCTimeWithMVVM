f7598ac8daf8f7a0b3dfd243302736a03ff0f241
Hosinging <pdhou23@gmail.com>
2023-04-05 00:17:36 +0900

    refactor: Model을 초기화 가능한 struct형태로 변경

--- pracUTCTimeWithMVVM/pracUTCTimeWithMVVM/View/Repository.swift
+++ pracUTCTimeWithMVVM/pracUTCTimeWithMVVM/View/Repository.swift
@@ -8,21 +8,14 @@
 import Foundation
 
 class Repository {
-	func fetchNow(onCompleted: @escaping (_ date: Date) -> Void) {
+	func fetchNow(onCompleted: @escaping (_ entity: UTCTimeModel) -> Void) {
 		guard let url = URL(string: "http://worldtimeapi.org/api/timezone/Asia/Seoul") else { return }
 		
-		URLSession.shared.dataTask(with: url, completionHandler: { [weak self] data, response, error in
+		URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
 			guard let data = data else { return }
 			guard let model = try? JSONDecoder().decode(UTCTimeModel.self, from: data) else { return }
 			
-			let formatter = DateFormatter()
-			formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX"
-			
-			guard let now = formatter.date(from: model.datetime) else { return }
-			
-			DispatchQueue.main.async {
-				onCompleted(now)
-			}
+			onCompleted(model)
 		}).resume()
 	}
 }
