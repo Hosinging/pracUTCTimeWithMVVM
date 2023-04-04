9b6864d5bb1de82134a5b7544603a33b4481a3af
Hosinging <pdhou23@gmail.com>
2023-04-04 22:39:13 +0900

    프로젝트 초기화 및 git 연동

---
+++ pracUTCTimeWithMVVM/pracUTCTimeWithMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pracUTCTimeWithMVVM
+//
+//  Created by Theo on 2023/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
