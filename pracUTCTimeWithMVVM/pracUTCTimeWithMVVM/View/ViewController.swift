89fb2124e8d77cf3f61562c964076949a255b4de
Hosinging <pdhou23@gmail.com>
2023-04-04 23:56:16 +0900

    feat: ViewModel에서 값을 초기화하는 함수 추가

--- pracUTCTimeWithMVVM/pracUTCTimeWithMVVM/View/ViewController.swift
+++ pracUTCTimeWithMVVM/pracUTCTimeWithMVVM/View/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+		viewModel.viewDidLoad()
 		dateTimeLabel.text = viewModel.dateTimeString
 	}
 	
